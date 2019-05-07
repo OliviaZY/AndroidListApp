be17e08fc193cd835a625bdb517584fe6c0e1466
Yuan zhou <zhouy4@rose-hulman.edu>
2019-05-07 10:21:33 -0500

    show list

--- app/src/main/java/com/example/listapp/MainActivity.java
+++ app/src/main/java/com/example/listapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.listapp;
 import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,5 +19,8 @@ public class MainActivity extends AppCompatActivity {
         Resources res = getResources();
         myListView = (ListView) findViewById(R.id.myListView);
         items = res.getStringArray(R.array.items);
+
+        myListView.setAdapter(new ArrayAdapter<String>(this,R.layout.my_listview_detail,items));
+
     }
 }
