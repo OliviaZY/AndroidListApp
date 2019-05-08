39e0103c2114649a846adb9086bc258f09e38535
Yuan zhou <zhouy4@rose-hulman.edu>
2019-05-08 16:57:00 -0500

    connect to second acitity that can show a picture

--- app/src/main/java/com/example/listapp/MainActivity.java
+++ app/src/main/java/com/example/listapp/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.listapp;
 
+import android.content.Intent;
 import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -23,16 +26,21 @@ public class MainActivity extends AppCompatActivity {
         Resources res = getResources();
         myListView = (ListView) findViewById(R.id.myListView);
         items = res.getStringArray(R.array.items);
-        //Log.d(getClass().getName(), "value = " + items[0]+" value2 "+items[1]+" value3 "+items[2]);
         prices = res.getStringArray(R.array.prices);
-        //Log.d(getClass().getName(), "value = " + prices[0]+" value2 "+prices[1]+" value3 "+prices[2]);
         descriptions = res.getStringArray(R.array.descriptions);
-        //Log.d(getClass().getName(), "value = " + descriptions[0]+" value2 "+descriptions[1]+" value3 "+descriptions[2]);
         // myListView.setAdapter(new ArrayAdapter<String>(this,R.layout.my_listview_detail,items));
 
         ItemAdapter itemAdapter = new ItemAdapter(this, items, prices, descriptions);
         //Log.d(getClass().getName(), "value = " + itemAdapter.descriptions[0]+" value2 "+itemAdapter.descriptions[1]+" value3 "+itemAdapter.descriptions[2]);
         myListView.setAdapter(itemAdapter);
 
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent showDetailActivity = new Intent(getApplicationContext(), DetailActivity.class);
+                showDetailActivity.putExtra("com.example.listapp.index", position);
+                startActivity(showDetailActivity);
+            }
+        });
     }
 }
